8b9cf8b9953ef83117ab39ed51d5525e7d630983
Legolass322 <l.e.g.o.l.a.s.s.dm@gmail.com>
2023-02-10 12:47:45 +0300

    add pages, code splitting

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,5 +4,6 @@ import type { Configuration as DevConfiguration } from 'webpack-dev-server'
 export default function buildDevServer(options: BuildOptions): DevConfiguration {
     return {
         port: options.port,
+        historyApiFallback: true,
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Counter from './components/Counter'
+import { Suspense } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './index.scss'
+import { AboutPageLazy as AboutPage } from './pages/AboutPage/AboutPage.lazy'
+import { MainPageLazy as MainPage } from './pages/MainPage/MainPage.lazy'
 
 export const App = () => {
   return (
     <div className='app'>
-        <Counter />
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
